da3131d7695c14ad37489ccbbe65dd27d8bd95fc
oldmadowl <oldmadowl@gmail.com>
2022-09-21 20:10:03 +0300

    finished button animation

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ const btn = document.querySelector('.present-btn');
 btn.addEventListener('click', () => {
   btn.classList.add('present-btn--clicked');
 })
+
+ 
+const svgLength = document.getElementById('present-path').getTotalLength();
